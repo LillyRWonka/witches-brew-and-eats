c02a9e569cf20b32c29cfb7808ea688cb18bfc15
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-04-28 13:22:29 -0700

    added in the routing information into the app js folder to include the search bar

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Search from "./components/Search";
 import Cart from "./pages/Cart";
 import UserAccount from "./pages/UserAccount";
 import Header from "./components/Header/index";
@@ -53,7 +54,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              
+              <Route path="/search" element={<Search/>} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/userAccount" element={<UserAccount />} />
               <Route path="/stripe" element={<Stripe />} />
