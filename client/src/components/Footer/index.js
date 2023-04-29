2a23350bc5bbdfacfa190f01c47b09c4f0193ce6
sploganathan <sakthiloganathan1127@gmail.com>
2023-04-29 01:07:24 -0700

    fix(go back): go back removed

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+
 import "./index.css";
 import culdron from "../../Assets/Couldron.png";
 const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
   return (
     <footer className="w-100  bg-secondary ">
       <div className="footer-container text-center mb-2">
-        {location.pathname !== "/" && (
-          <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
-            &larr; Go Back
-          </button>
-        )}
         <h4 className="no-margin text-bold">
           Bippity boppity boo,{" "}
           <span
