b7ce07c94bece8b5d3fc9ccea08d5f84783db10e
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-04-29 14:03:41 -0700

    add Nutrition API fetch to utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,10 @@
 //EC: function make a search to EDAMAM api
-
 const appKey = process.env.EDAMAM_APP_KEY;
 const appId = process.env.EDAMAM_APP_ID;
 
 require('dotenv').config();
 
+export const getNutritionAPI = (ingredients) => {
+    const query = encodeURIComponent(ingredients);
+    return fetch(`https://api.edamam.com/api/nutrition-data?app_id=${appId}&app_key=${appKey}&nutrition-type=cooking&ingr=${query}`);
+  };
