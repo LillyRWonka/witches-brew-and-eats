76a0b3dc3c7dc4796ccdc4fc0200ca3ac42c1f0c
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-04-28 21:03:04 -0700

    Download pdf for Recipe

--- client/src/components/RecipesList.js
+++ client/src/components/RecipesList.js
@@ -10,7 +10,9 @@ function Recipe() {
         <h2 className='card-title'>This is a Recipe</h2>
         <p className='card-description'>This is a description</p>
       </div>
-      <button className='card-btn'>View Recipe</button>
+      <button className='card-btn'>
+      <a href={`${process.env.PUBLIC_URL}/pdfs/Easy_recipe.pdf`} target="_blank" rel="noopener noreferrer">Download PDF</a>
+      </button>
     </div>
   )
   }
